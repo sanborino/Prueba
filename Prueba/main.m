c2c8e918ab1c50d6ed107bacd904a1d48d36f3a7
sanborino <Ricardo@MacBook-Pro-de-Ricardo.local>
2015-06-07 21:52:15 -0600

    Initial Commit

---
+++ Prueba/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Prueba
+//
+//  Created by Ricardo A Mondragon A on 07/06/15.
+//  Copyright (c) 2015 Ricardo A Mondragon A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
